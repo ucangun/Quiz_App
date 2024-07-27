1d92c530addddc558669edd759ec80e531e90c2c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-27 20:59:01 +0200

    "Added Bootstrap, updated App.js and Home.jsx with new styles and layout"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 import AppRouter from "./router/AppRouter";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={`${styles.section_home} p-3`}>
+      <div
+        className={`${styles.container}  d-flex flex-column align-items-center gap-3`}
+      >
+        <h1>Welcome to IT Master Quiz!</h1>
+        <p>Get ready to challenge your mind and expand your knowledge.</p>
+        <button>Let's Start</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
