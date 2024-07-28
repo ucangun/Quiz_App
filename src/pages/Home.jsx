c128f93c5a026d48ab87bb5bb234c5aea81eff97
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-28 15:07:28 +0200

    "Added video background to Home page and updated styles"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,21 @@ const Home = () => {
   return (
     <div className={`${styles.section_home} p-3`}>
       <div
-        className={`${styles.container}  d-flex flex-column align-items-center gap-3`}
+        className={`${styles.container} d-flex flex-column align-items-center gap-3`}
       >
+        <div className={styles.video_container}>
+          <video
+            autoPlay
+            muted
+            loop
+            className={styles.background_video}
+            playbackRate="0.2"
+          >
+            <source src="/videos/video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div className={styles.video_overlay}></div>
+        </div>
         <h1>Welcome to IT Master Quiz!</h1>
         <p>Get ready to challenge your mind and expand your knowledge.</p>
         <button onClick={() => navigate("/quiz")}>Let's Start</button>
